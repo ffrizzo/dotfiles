bf0fae6e54dc669dc623b2856d8d89ce0b42d18c
Fabiano Frizzo <ffrizzo@gmail.com>
2020-06-07 15:06:35 -0300

    Update to use brew bundle on install

---
+++ Brewfile
@@ -0,0 +1,101 @@
+#tap hombrew cask-versions and completions
+tap 'homebrew/cask-drivers'
+tap 'homebrew/cask-versions'
+tap 'caskroom/fonts'
+
+#install hombrew formulas
+brew 'gettext'
+brew 'fd'
+brew 'jq'
+brew 'yq'
+brew 'jump'
+brew 'yamllint'
+brew 'dos2unix'
+brew 'git'
+brew 'mercurial'
+brew 'wget'
+brew 'go'
+brew 'pyenv'
+brew 'pyenv-virtualenv'
+brew 'nvm'
+brew 'yarn'
+brew 'httpie'
+brew 'hugo'
+brew 'trash'
+brew 'upx'
+
+brew 'golangci/tap/golangci-lint'
+brew 'goreleaser/tap/goreleaser'
+
+brew 'awscli'
+brew 'TylerBrock/saw'
+brew 'packer'
+brew 'ansible'
+brew 'tree'
+brew 'travis'
+brew 'gitlab-runner'
+
+brew 'warrensbox/tap/tfswitch'
+brew 'warrensbox/tap/tgswitch'
+
+brew 'mas'
+brew 'dockutil'
+
+cask 'authy'
+
+cask 'google-chrome-canary'
+cask 'firefox-developer-edition'
+cask 'opera-developer'
+cask 'tor-browser'
+cask 'brave-browser'
+cask 'transmission'
+
+cask 'google-hangouts'
+cask 'spotify'
+cask 'the-unarchiver'
+cask 'vlc'
+cask 'skype'
+cask 'slack-beta'
+cask 'keybase'
+cask 'harvest'
+cask 'discord'
+cask 'blue-jeans'
+cask 'zoomus'
+cask 'microsoft-outlook'
+cask 'logitech-camera-settings'
+
+cask 'java'
+brew 'git-credential-manager'
+
+cask 'shiftit'
+cask 'iterm2'
+cask 'ngrok'
+
+cask 'balenaetcher'
+cask 'arduino'
+cask 'visual-studio-code'
+cask 'intellij-idea-ce'
+cask 'paw'
+
+cask 'sourcetree'
+cask 'diffmerge'
+
+brew 'kubernetes-cli'
+brew 'helm'
+brew 'kubectx'
+brew 'kops'
+brew 'k3d'
+brew 'istioctl'
+
+cask 'lens'
+cask 'spin'
+
+cask 'docker'
+
+#install homebrew completions
+brew 'zsh-completion'
+
+cask 'font-source-code-pro-for-powerline'
+cask 'font-source-code-pro'
+cask 'font-source-sans-pro'
+cask 'font-source-serif-pro'
--- install-homebrew.sh
+++ install-homebrew.sh
@@ -1,112 +1,14 @@
 #!/bin/zsh
-echo ""
-echo "Instaling Homebrew"
+printf "\nInstaling Homebrew\n"
 export HOMEBREW_CASK_OPTS="--appdir=/Applications"
 
 #installl homebrew
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
 
-echo ""
-echo "Instaling applications from homebrew"
+printf "\nInstaling applications from homebrew\n"
 
-#tap hombrew cask-versions and completions
-brew tap homebrew/cask-drivers
-brew tap homebrew/cask-versions
-brew tap caskroom/fonts
+brew tap homebrew/bundle
+brew bundle
 
-#install hombrew formulas
-brew install gettext
-brew install fd
-brew install jq
-brew install yq
-brew install yamllint
-brew install dos2unix
-brew install git
-brew install mercurial
-brew install wget
-brew install go
-brew install pyenv
-brew install pyenv-virtualenv
-brew install nvm
-brew install yarn
-brew install httpie
-brew install hugo
-brew install trash
-brew install upx
-
-brew install golangci/tap/golangci-lint
-brew install goreleaser/tap/goreleaser
-
-brew install awscli
-brew install TylerBrock/saw
-brew install packer
-brew install ansible
-brew install tree
-brew install travis
-brew install gitlab-runner
-
-brew install warrensbox/tap/tfswitch
-brew install warrensbox/tap/tgswitch
-
-brew install mas
-brew install dockutil
-
-brew cask install authy
-
-brew cask install google-chrome-canary
-brew cask install firefox-developer-edition
-brew cask install opera-developer
-brew cask install tor-browser
-brew cask install brave-browser
-brew cask install transmission
-
-brew cask install google-hangouts
-brew cask install spotify
-brew cask install the-unarchiver
-brew cask install vlc
-brew cask install skype
-brew cask install slack-beta
-brew cask install keybase
-brew cask install harvest
-brew cask install blue-jeans
-brew cask install zoomus
-brew cask install microsoft-outlook
-
-brew cask install java
-brew install git-credential-manager
-
-brew cask install shiftit
-brew cask install iterm2
-
-brew cask install balenaetcher
-brew cask install arduino
-brew cask install visual-studio-code
-brew cask install paw
-
-brew cask install sourcetree
-brew cask install diffmerge
-
-brew install kubernetes-cli
-brew install helm
-brew install kubectl
-brew install kubectx
-brew install kops
-brew install k3d
-
-brew cask install lens
-brew cask install spin
-
-brew cask install docker
-brew cask install minikube
-
-#install homebrew completions
-brew install zsh-completion
-
-brew cask install font-source-code-pro-for-powerline
-brew cask install font-source-code-pro
-brew cask install font-source-sans-pro
-brew cask install font-source-serif-pro
-
-echo ""
-echo "Cleanup brew formulas"
+printf "\nCleanup brew formulas\n"
 brew cleanup
