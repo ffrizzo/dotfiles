d410c54a8877252a5ca2a951a06dd6507535ec2c
Fabiano Frizzo <ffrizzo@gmail.com>
2022-11-18 16:23:21 -0300

    Update dockutil to latest version Update setup-dock.py to python3

--- setup-dock.py
+++ setup-dock.py
@@ -7,10 +7,13 @@ This script configures the Dock content the
 way I like it. Use with caution...
 
 Hannes Juutilainen <hjuutilainen@mac.com>
+
+Update to python3 by
+Fabiano Frizzo <ffrizzo@gmail.com>
 """
 
+import io
 import sys
-import getopt
 import os
 import subprocess
 import plistlib
@@ -24,7 +27,7 @@ appleApps = [
     "/System/Applications/Mission Control.app",
     "/System/Applications/System Preferences.app",
     "/Applications/Safari.app"
-    ]
+]
 
 # =======================================
 # Optional Applications
@@ -32,51 +35,59 @@ appleApps = [
 # =======================================
 thirdPartyApps = [
     {
-    "path": "/Applications/Visual Studio Code.app",
-    "args": [ "--after", "System Preferences" ],
-    "forced": True
+        "path": "/Applications/Visual Studio Code.app",
+        "args": ["--after", "System Preferences"],
+        "forced": True
     },
     {
-    "path": "/Applications/iTerm.app",
-    "args": [ "--after", "System Preferences" ],
-    "forced": True
-    },    
-    {
-    "path": "/Applications/Spotify.app",
-    "args": [ "--after", "Safari" ],
-    "forced": True
+        "path": "/Applications/iTerm.app",
+        "args": ["--after", "System Preferences"],
+        "forced": True
     },
     {
-    "path": "/Applications/Slack.app",
-    "args": [ "--after", "Safari" ],
-    "forced": True
+        "path": "/Applications/Spotify.app",
+        "args": ["--after", "Safari"],
+        "forced": True
     },
     {
-    "path": "/Applications/Firefox Developer Edition.app",
-    "args": [ "--after", "Safari" ],
-    "forced": True
+        "path": "/Applications/Slack.app",
+        "args": ["--after", "Safari"],
+        "forced": True
     },
     {
-    "path": "/Applications/Google Chrome Canary.app",
-    "args": [ "--after", "Safari" ],
-    "forced": True
+        "path": "/Applications/Discord.app",
+        "args": ["--after", "Safari"],
+        "forced": True
     },
     {
-    "path": "/Applications/Brave Browser.app",
-    "args": [ "--after", "Safari" ],
-    "forced": True
+        "path": "/Applications/Firefox Developer Edition.app",
+        "args": ["--after", "Safari"],
+        "forced": True
+    },
+    {
+        "path": "/Applications/Google Chrome Canary.app",
+        "args": ["--after", "Safari"],
+        "forced": True
+    },
+    {
+        "path": "/Applications/Brave Browser.app",
+        "args": ["--after", "Safari"],
+        "forced": True
     }
-    ]
+]
 
 dockutilPath = ""
+
 
 class Usage(Exception):
     def __init__(self, msg):
         self.msg = msg
 
+
 def dockutilExists():
     whichProcess = ["which", "dockutil"]
-    p = subprocess.Popen(whichProcess, bufsize=1, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    p = subprocess.Popen(whichProcess, bufsize=1,
+                         stdout=subprocess.PIPE, stderr=subprocess.PIPE)
     (path, err) = p.communicate()
     if os.path.exists(path.strip()):
         global dockutilPath
@@ -85,33 +96,40 @@ def dockutilExists():
     else:
         return False
 
-def removeEverything( restartDock=False ):
+
+def removeEverything(restartDock=False):
     dockutilProcess = [dockutilPath, "--remove", "all"]
     if not restartDock:
         dockutilProcess.append("--no-restart")
-    p = subprocess.Popen(dockutilProcess, bufsize=1, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    p = subprocess.Popen(dockutilProcess, bufsize=1,
+                         stdout=subprocess.PIPE, stderr=subprocess.PIPE)
     (output, err) = p.communicate()
     pass
+
 
 def dockutilAdd(aPath, args):
     dockutilProcess = [dockutilPath, "--add", aPath]
     if args:
         dockutilProcess = dockutilProcess + args
     dockutilProcess.append("--no-restart")
-    p = subprocess.Popen(dockutilProcess, bufsize=1, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    p = subprocess.Popen(dockutilProcess, bufsize=1,
+                         stdout=subprocess.PIPE, stderr=subprocess.PIPE)
     (output, err) = p.communicate()
     pass
+
 
 def localDisks():
     """Run diskutil list -plist """
     diskutilProcess = ["diskutil", "list", "-plist"]
-    p = subprocess.Popen(diskutilProcess, bufsize=1, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    p = subprocess.Popen(diskutilProcess, bufsize=1,
+                         stdout=subprocess.PIPE, stderr=subprocess.PIPE)
     (output, err) = p.communicate()
     if output != "":
-        outputPlist = plistlib.readPlistFromString(output)
+        outputPlist = plistlib.load(io.BytesIO(output))
         return outputPlist['VolumesFromDisks']
     else:
         return None
+
 
 def addFolders():
     """
@@ -125,8 +143,7 @@ def addFolders():
     Add everything that exists.
     """
     username = getpass.getuser()
-    pathsToCheckInDisks = [ "Users", "home" ]
-    foldersToAdd = [ "Documents", "Downloads" ]
+    pathsToCheckInDisks = ["Users", "home"]
     for aDisk in localDisks():
         diskPath = os.path.join("/Volumes", aDisk)
         for aPath in pathsToCheckInDisks:
@@ -135,25 +152,25 @@ def addFolders():
             documents = os.path.join(homePath, "Documents")
             downloads = os.path.join(homePath, "Downloads")
             if os.path.exists(documents):
-                print "Adding %s" % documents
                 label = "Documents"
                 args = [
-                    "--view", "fan",
+                    "--view", "grid",
                     "--display", "stack",
                     "--sort", "name",
                     "--label", label
-                    ]
+                ]
                 dockutilAdd(documents, args)
+                print("Added %s" % documents)
             if os.path.exists(downloads):
-                print "Adding %s" % downloads
                 label = "Downloads"
                 args = [
-                    "--view", "fan",
+                    "--view", "grid",
                     "--display", "stack",
                     "--sort", "dateadded",
                     "--label", label
-                    ]
+                ]
                 dockutilAdd(downloads, args)
+                print("Added %s" % downloads)
 
 
 def main(argv=None):
@@ -161,44 +178,45 @@ def main(argv=None):
         argv = sys.argv
     try:
         if not dockutilExists():
-            print "dockutil not found"
-            print "Get it from https://github.com/kcrawford/dockutil"
-            print "or run \"git clone https://github.com/kcrawford/dockutil.git\""
+            print("dockutil not found")
+            print("Get it from https://github.com/kcrawford/dockutil")
+            print("or run \"git clone https://github.com/kcrawford/dockutil.git\"")
             return 1
 
-        confirmation = raw_input("Are you sure? y/n: ").lower()
+        confirmation = input("Are you sure? y/n: ").lower()
         if confirmation == 'y':
-            print "Continuing..."
+            print("Continuing...")
         elif confirmation == '' or confirmation == 'n':
             raise Usage("Exiting...")
         else:
-            print 'Please enter y or n.'
+            print('Please enter y or n.')
             return 1
 
         # Start with an empty Dock
-        removeEverything( restartDock=False );
+        removeEverything(restartDock=False)
 
         # Add standard Apple apps
         for anApp in appleApps:
             dockutilAdd(anApp, None)
-            #print "Added %s" % anApp
+            print("Added %s" % anApp)
 
         # Add 3rd party apps
         for anApp in thirdPartyApps:
             if os.path.exists(anApp["path"]) or anApp["forced"]:
                 dockutilAdd(anApp["path"], anApp["args"])
-                #print "Added %s" % anApp["path"]
+                print("Added %s" % anApp["path"])
             else:
-                print "Skipped %s" % anApp["path"]
+                print("Skipped %s" % anApp["path"])
 
         # Add folders
         addFolders()
 
-        print "Done. You might want to restart Dock by running \"killall Dock\""
+        print("Done. You might want to restart Dock by running \"killall Dock\"")
 
-    except Usage, err:
-        print >> sys.stderr, str(err.msg)
+    except Usage as err:
+        print(">> %s\n" % err, file=sys.stderr)
         return 2
+
 
 if __name__ == "__main__":
     sys.exit(main())
--- setup.sh
+++ setup.sh
@@ -15,11 +15,15 @@ yes | cp -a ./home/ ~/
 mkdir -p ~/.nvm
 mkdir -p ~/.pyenv
 
-sh install-zsh.sh      
+sh install-zsh.sh
 
 echo "\nInstalling Fonts"
 open -a Font\ Book ./fonts/*.ttf
 
 echo "\nSetup apps on dock..."
-python setup-dock.py
+export DOCKUTIL_VERSION=3.0.2
+curl -L https://github.com/kcrawford/dockutil/releases/download/${DOCKUTIL_VERSION}/dockutil-${DOCKUTIL_VERSION}.pkg --output dockutil-${DOCKUTIL_VERSION}.pkg
+sudo installer -pkg dockutil-${DOCKUTIL_VERSION}.pkg -target /
+python3 setup-dock.py
 killall Dock
+rm -rf dockutil-${DOCKUTIL_VERSION}.pkg
