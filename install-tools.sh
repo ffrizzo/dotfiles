334f0ba6d32ac731d063837dad70b9084bfa6ac9
Fabiano Frizzo <ffrizzo@gmail.com>
2022-11-21 16:07:23 -0300

    Update install-tools.sh to download for linux/mac

--- install-tools.sh
+++ install-tools.sh
@@ -1,5 +1,8 @@
 #!/bin/zsh
 
 echo "\nInstalling Spin CLI..."
-curl -o /usr/local/bin/spin https://storage.googleapis.com/spinnaker-artifacts/spin/$(curl -s https://storage.googleapis.com/spinnaker-artifacts/spin/latest)/darwin/amd64/spin
+OS=$(uname -s | tr '[:upper:]' '[:lower:]')
+ARCH=$(uname -m)
+VERSION=$(curl -s https://storage.googleapis.com/spinnaker-artifacts/spin/latest)
+curl -o /usr/local/bin/spin https://storage.googleapis.com/spinnaker-artifacts/spin/$VERSION/$OS/$ARCH/spin
 chmod +x /usr/local/bin/spin
